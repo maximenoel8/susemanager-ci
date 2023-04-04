699746b0f25de7ff46292d0149bbdb765efca40f
Maxime Noel <maxime.noel@suse.com>
2023-04-04 13:37:33 +1200

    Update the last changes

--- jenkins_pipelines/environments/common/pipeline-build-validation.groovy
+++ jenkins_pipelines/environments/common/pipeline-build-validation.groovy
@@ -205,46 +205,46 @@ def run(params) {
             }
         }
         finally {
-//            stage('Save TF state') {
-//                archiveArtifacts artifacts: "results/sumaform/terraform.tfstate, results/sumaform/.terraform/**/*"
-//            }
-//
-//            stage('Get results') {
-//                def result_error = 0
-//                if (deployed || !params.must_deploy) {
-//                    try {
-//                        sh "./terracumber-cli ${common_params} --logfile ${resultdirbuild}/testsuite.log --runstep cucumber --cucumber-cmd 'export BUILD_VALIDATION=true; cd /root/spacewalk/testsuite; rake cucumber:build_validation_finishing'"
-//                    } catch(Exception ex) {
-//                        println("ERROR: rake cucumber:build_validation_finishing failed")
-//                        result_error = 1
-//                    }
-//                    try {
-//                        sh "./terracumber-cli ${common_params} --logfile ${resultdirbuild}/testsuite.log --runstep cucumber --cucumber-cmd 'export BUILD_VALIDATION=true; cd /root/spacewalk/testsuite; rake utils:generate_test_report'"
-//                    } catch(Exception ex) {
-//                        println("ERROR: rake utils:generate_test_report failed")
-//                        result_error = 1
-//                    }
-//                    sh "./terracumber-cli ${common_params} --logfile ${resultdirbuild}/testsuite.log --runstep getresults"
-//                    publishHTML( target: [
-//                            allowMissing: true,
-//                            alwaysLinkToLastBuild: false,
-//                            keepAll: true,
-//                            reportDir: "${resultdirbuild}/cucumber_report/",
-//                            reportFiles: 'cucumber_report.html',
-//                            reportName: "Build Validation report"]
-//                    )
-//                    // junit allowEmptyResults: true, testResults: "${junit_resultdir}/*.xml"
-//                }
-//                // Send email
-//                sh "./terracumber-cli ${common_params} --logfile ${resultdirbuild}/mail.log --runstep mail"
-//                // Clean up old results
-//                sh "./clean-old-results -r ${resultdir}"
-//                // Fail pipeline if client stages failed
-//                if (env.client_stage_result_fail) {
-//                    error("Client stage failed")
-//                }
-//                sh "exit ${result_error}"
-//            }
+            stage('Save TF state') {
+                archiveArtifacts artifacts: "results/sumaform/terraform.tfstate, results/sumaform/.terraform/**/*"
+            }
+
+            stage('Get results') {
+                def result_error = 0
+                if (deployed || !params.must_deploy) {
+                    try {
+                        sh "./terracumber-cli ${common_params} --logfile ${resultdirbuild}/testsuite.log --runstep cucumber --cucumber-cmd 'export BUILD_VALIDATION=true; cd /root/spacewalk/testsuite; rake cucumber:build_validation_finishing'"
+                    } catch(Exception ex) {
+                        println("ERROR: rake cucumber:build_validation_finishing failed")
+                        result_error = 1
+                    }
+                    try {
+                        sh "./terracumber-cli ${common_params} --logfile ${resultdirbuild}/testsuite.log --runstep cucumber --cucumber-cmd 'export BUILD_VALIDATION=true; cd /root/spacewalk/testsuite; rake utils:generate_test_report'"
+                    } catch(Exception ex) {
+                        println("ERROR: rake utils:generate_test_report failed")
+                        result_error = 1
+                    }
+                    sh "./terracumber-cli ${common_params} --logfile ${resultdirbuild}/testsuite.log --runstep getresults"
+                    publishHTML( target: [
+                            allowMissing: true,
+                            alwaysLinkToLastBuild: false,
+                            keepAll: true,
+                            reportDir: "${resultdirbuild}/cucumber_report/",
+                            reportFiles: 'cucumber_report.html',
+                            reportName: "Build Validation report"]
+                    )
+                    // junit allowEmptyResults: true, testResults: "${junit_resultdir}/*.xml"
+                }
+                // Send email
+                sh "./terracumber-cli ${common_params} --logfile ${resultdirbuild}/mail.log --runstep mail"
+                // Clean up old results
+                sh "./clean-old-results -r ${resultdir}"
+                // Fail pipeline if client stages failed
+                if (env.client_stage_result_fail) {
+                    error("Client stage failed")
+                }
+                sh "exit ${result_error}"
+            }
         }
     }
 }
@@ -275,17 +275,14 @@ def clientTestingStages() {
             if (params.must_add_MU_repositories) {
                 stage("Add_MUs_${minion}") {
                     if (minion.contains('ssh_minion')) {
-                        def minion_name = minion.replaceAll('ssh_minion', 'minion')
-                        echo "Print dictionnary ${mu_sync_status}"
-                        echo "Print minion replace ${minion.replaceAll('ssh_minion', 'minion')}"
-                        echo "SSH dictio ${mu_sync_status[minion_name]}"
+                        // SSH minion need minion MU channel. This section wait until minion finish creating MU channel
+                        def minion_name_without_ssh = minion.replaceAll('ssh_minion', 'minion')
+                        println "Waiting for the MU channel creation by ${minion_name_without_ssh} for ${minion}."
                         waitUntil {
-                            echo "SSH dictio in wait ${mu_sync_status[minion_name]}"
-                            mu_sync_status[minion_name]
+                            mu_sync_status[minion_name_without_ssh]
                         }
-                        echo "MU repository created by ${minion}"
+                        println "MU channel available for ${minion} "
                     } else {
-                        println("Create group ${minion}")
                         if (params.confirm_before_continue) {
                             input 'Press any key to start adding Maintenance Update repositories'
                         }
@@ -295,14 +292,13 @@ def clientTestingStages() {
                             error("Add custom channels and MU repositories failed with status code: ${res_mu_repos}")
                         }
                         echo "Custom channels and MU repositories status code: ${res_mu_repos}"
-                        res_sync_mu_repos = sh(script: "./terracumber-cli ${common_params} --logfile ${resultdirbuild}/testsuite.log --runstep cucumber --cucumber-cmd 'unset ${temporaryList.join(' ')}; export BUILD_VALIDATION=true; cd /root/spacewalk/testsuite; rake cucumber:build_validation_wait_for_custom_reposync'", returnStatus: true)
+                        res_sync_mu_repos = sh(script: "./terracumber-cli ${common_params} --logfile ${resultdirbuild}/testsuite.log --runstep cucumber --cucumber-cmd 'export NODE=${minion}; unset ${temporaryList.join(' ')}; export BUILD_VALIDATION=true; cd /root/spacewalk/testsuite; rake cucumber:build_validation_wait_for_custom_reposync'", returnStatus: true)
                         echo "Custom channels and MU repositories synchronization status code: ${res_sync_mu_repos}"
                         if (res_sync_mu_repos != 0) {
                             error("Custom channels and MU repositories synchronization failed with status code: ${res_sync_mu_repos}")
                         }
+                        // Update minion repo sync status variable once the MU channel is synchronized
                         mu_sync_status[minion] = true
-                        echo "Dictionnary ${mu_sync_status}"
-                        echo "Dictionnary value ${mu_sync_status[minion]}"
                     }
                 }
             }
@@ -320,7 +316,7 @@ def clientTestingStages() {
                         if (res_non_MU_repositories != 0) {
                             error("Add common channels failed with status code: ${res_non_MU_repositories}")
                         }
-                        res_sync_non_MU_repositories = sh(script: "./terracumber-cli ${common_params} --logfile ${resultdirbuild}/testsuite.log --runstep cucumber --cucumber-cmd 'unset ${temporaryList.join(' ')}; export BUILD_VALIDATION=true; cd /root/spacewalk/testsuite; rake cucumber:build_validation_wait_for_custom_reposync'", returnStatus: true)
+                        res_sync_non_MU_repositories = sh(script: "./terracumber-cli ${common_params} --logfile ${resultdirbuild}/testsuite.log --runstep cucumber --cucumber-cmd 'export NODE=${minion}; unset ${temporaryList.join(' ')}; export BUILD_VALIDATION=true; cd /root/spacewalk/testsuite; rake cucumber:build_validation_wait_for_custom_reposync'", returnStatus: true)
                         echo "Non MU Repositories synchronization status code: ${res_sync_non_MU_repositories}"
                         if (res_sync_non_MU_repositories != 0) {
                             error("Non MU Repositories synchronization failed with status code: ${res_sync_non_MU_repositories}")
@@ -408,12 +404,12 @@ def getMinionList() {
     moduleList.each { lane ->
         def instanceList = lane.tokenize(".")
         if (instanceList[1].contains('minion') || instanceList[1].contains('client')) {
-            nodeList.add(instanceList[1].replaceAll("-", "_").replaceAll("sshminion", "ssh_minion").replaceAll("sles", "sle"))
-            envVar.add(instanceList[1].replaceAll("-", "_").replaceAll("sles", "sle").toUpperCase())
+            nodeList.add(instanceList[1].replaceAll('-', '_').replaceAll('sshminion', 'ssh_minion').replaceAll('sles', 'sle'))
+            envVar.add(instanceList[1].replaceAll('-', '_').replaceAll('sles', 'sle').toUpperCase())
         }
     }
     // Convert jenkins minions list parameter to list
-    Set<String> nodesToRun = params.minions_to_run.split(", ")
+    Set<String> nodesToRun = params.minions_to_run.split(', ')
     // Create a variable with declared nodes on Jenkins side but not deploy and print it
     def notDeployedNode = nodesToRun.findAll { !nodeList.contains(it) }
     println "This minions are declared in jenkins but not deployed ! ${notDeployedNode}"
@@ -421,9 +417,11 @@ def getMinionList() {
     // This difference will be the nodes to disable
     def disabledNodes = nodeList.findAll { !nodesToRun.contains(it) }
     // Convert this list to cucumber compatible environment variable
-    def envVarDisabledNodes = disabledNodes.collect { it.replaceAll("ssh_minion", "sshminion").toUpperCase() }
+    def envVarDisabledNodes = disabledNodes.collect { it.replaceAll('ssh_minion', 'sshminion').toUpperCase() }
     // Create a node list without the disabled nodes. ( use to configure the client stage )
     def nodeListWithDisabledNodes = nodeList - disabledNodes
+    // Create a map storing mu synchronization state for each minion.
+    // This map is to be sure ssh minions have the MU channel ready.
     for (node in nodeListWithDisabledNodes ) {
         MUSyncStatus[node] = false
     }
