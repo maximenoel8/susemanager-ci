5b7943fd545d689e17105eca5cb9c13c1893066c
Maxime Noel <maxime.noel@suse.com>
2023-08-03 16:15:26 +1200

    Create temporary branch to test changes

--- jenkins_pipelines/environments/common/pipeline-build-validation.groovy
+++ jenkins_pipelines/environments/common/pipeline-build-validation.groovy
@@ -273,6 +273,7 @@ def run(params) {
                     )
                     // junit allowEmptyResults: true, testResults: "${junit_resultdir}/*.xml"
                 }
+
                 // Send email
                 sh "./terracumber-cli ${common_params} --logfile ${resultdirbuild}/mail.log --runstep mail"
                 // Clean up old results
