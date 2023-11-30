b467a8e8428e5d8930f7471564d4f95d6d40ac3f
Maxime Noel <maxime.noel@suse.com>
2023-12-01 10:56:02 +1300

    Add paygo variable

--- terracumber_config/tf_files/SUSEManager-4.3-build-validation-paygo-AWS.tf
+++ terracumber_config/tf_files/SUSEManager-4.3-build-validation-paygo-AWS.tf
@@ -172,6 +172,7 @@ module "server" {
   publish_private_ssl_key        = false
   use_os_released_updates        = false
   disable_download_tokens        = false
+  is_paygo_instance              = true
   ssh_key_path            = "./salt/controller/id_rsa.pub"
   provider_settings = {
     instance_type = "m6a.xlarge"
