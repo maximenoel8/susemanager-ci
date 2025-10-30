0c73d441ba9043ea53c2b2d2d8569dd3c9e8ae86
Maxime Noel <maxime.noel@suse.com>
2025-10-30 14:51:24 +1300

    bump build host to sles15sp7

--- terracumber_config/tf_files/personal/main_51.tf
+++ terracumber_config/tf_files/personal/main_51.tf
@@ -32,7 +32,7 @@ module "cucumber_testsuite" {
   cc_username = var.SCC_USER
   cc_password = var.SCC_PASSWORD
 
-  images = ["rocky8o", "opensuse155o", "opensuse156o", "ubuntu2404o", "sles15sp4o", "slmicro61o"]
+  images = ["rocky8o", "opensuse155o", "opensuse156o", "ubuntu2404o", "sles15sp4o", "sles15sp7o", "slmicro61o"]
 
   use_avahi    = false
   name_prefix   = "${var.ENVIRONMENT}-"
@@ -126,7 +126,7 @@ module "cucumber_testsuite" {
       }
     }
     build_host = {
-      image = "sles15sp4o"
+      image = "sles15sp7o"
       provider_settings = {
         mac    = var.ENVIRONMENT_CONFIGURATION[var.ENVIRONMENT].mac["build-host"]
         vcpu = 2
@@ -134,7 +134,7 @@ module "cucumber_testsuite" {
       }
     }
     pxeboot_minion = {
-      image = "sles15sp4o"
+      image = "sles15sp7o"
     }
     dhcp_dns = {
       name = "dhcp-dns"
