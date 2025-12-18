80bcb60360e160d54037c0de30c45e99584524f7
Maxime Noel <maxime.noel@suse.com>
2025-12-19 11:44:11 +1300

    Update profile

--- terracumber_config/tf_files/personal/main_43.tf
+++ terracumber_config/tf_files/personal/main_43.tf
@@ -39,7 +39,7 @@ module "cucumber_testsuite" {
   auth_registry          = "registry.mgr.suse.de:5000/cucutest"
   auth_registry_username = "cucutest"
   auth_registry_password = "cucusecret"
-  git_profiles_repo      = "https://github.com/uyuni-project/uyuni.git#:testsuite/features/profiles/internal_nue"
+  git_profiles_repo      = "https://github.com/uyuni-project/uyuni.git#:testsuite/features/profiles/temporary"
 
   mirror                   = "minima-mirror-ci-bv.mgr.suse.de"
   use_mirror_images        = true
