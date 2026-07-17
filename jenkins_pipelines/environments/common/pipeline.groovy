a3209851ffc5e5fd3e623ecb1dc0d2b3db95eaf6
Maxime Noel <maxime.noel@suse.com>
2026-07-17 14:48:45 +1200

    Try new declaration

--- jenkins_pipelines/environments/common/pipeline.groovy
+++ jenkins_pipelines/environments/common/pipeline.groovy
@@ -88,6 +88,7 @@ def run(params) {
                 if (mirror_scope != null) {
                     sh "ssh root@minima-mirror-ci-bv.`hostname -d` -t \"test -x /usr/local/bin/minima-${mirror_scope}.sh && /usr/local/bin/minima-${mirror_scope}.sh || echo 'no mirror script for this scope'\""
                 }
+                echo runDeploy
             }
 
             stage('Deploy') {
